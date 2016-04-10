589c1298888fe89a8d341339f6f0dd34207ff2f0
Domagoj <dpordan@gmail.com>
2016-04-10 13:18:40 +0200

    Working click slider with CSS3

--- transition.js
+++ transition.js
@@ -1,41 +1,52 @@
 $(document).ready(function() {
-    $('.transitionobj li:first').before($('.transitionobj li:last'));
+    var width = 400;
+    var array = [-1, 0, 1, 2];
+    function calculateArray() {
+      for (var i = 0; i < array.length; i++) {
+        if (array[i] < 0) {
+          array[i] = 2;
+          continue;
+        }
+        array[i] -= 1;
+      }
+    }
+    $('.transitionobj').children().each(function(index) {
+      var value = index * width;
+      $(this).css({left:value + 'px'});
+    });
+    $('.transitionobj li:last').css({left: '-' + width + 'px'});
     $('.mybutton').on("click", function() {
       moveIt();
       //jMove();
       //setInterval(moveIt, 2000);
     });
-    var i = 0;
     function moveIt() {
-      
-      var temp;
       var t;
-     if (i < 3) {
         $('.transitionobj').bind("transitionend", function() {
           var el = $(this)[0];
-          $('.transitionobj li:last').after($('.transitionobj li:first'));
+          $(this).children().each(function(index) {
+            var value = array[index] * width;
+            $(this).css({left: value + 'px'});
+          });
+          calculateArray();
           $('.transitionobj').removeClass('transitionClass');
           console.log("callback: Changed list");
-          def = "translateX(-400px)";
+          def = "translateX(0px)";
           el.style.transform = def;
-          //$('.transitionobj').css({'transform': def});
           $('.transitionobj').unbind("transitionend");
           console.log("callback: Css to default");
-          setTimeout(moveIt, 0);
-          //moveIt();
+          //setTimeout(moveIt, 0);
         });
-       
         $('.transitionobj').addClass('transitionClass');
-        prop = "translateX(-800px)";
+        prop = "translateX(-400px)";
         temp = $('.transitionobj')[0];
         t = temp.style.transform;
         console.log('base: configured transform:' + t);
 
         console.log("base: Initiate new configuration.");
         
-          $('.transitionobj').css({'transform': prop});
-          i++;
-      }
+        $('.transitionobj').css({'transform': prop});
+
     }
     function jMove() {
 
